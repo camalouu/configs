0ada2bced8075fcb99949578cc7f9d802070deaf
viktor <camalouu@gmail.com>
2025-12-25 22:00:59 +0100

    add nixos-gnome-xorg config

--- .zshrc
+++ .zshrc
@@ -1,32 +1,27 @@
-export EDITOR=nvim
-alias ll="lsd -la"
-alias ls="lsd"
-
-if [[ -e /usr/share/zsh/manjaro-zsh-config ]]; then
-  source /usr/share/zsh/manjaro-zsh-config
-fi
-
-# source /usr/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
-
+# Yazi cd function
 function y() {
-	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-	yazi "$@" --cwd-file="$tmp"
-	if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-		builtin cd -- "$cwd"
-	fi
-	rm -f -- "$tmp"
+  local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+  yazi "$@" --cwd-file="$tmp"
+  if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+    builtin cd -- "$cwd"
+  fi
+  rm -f -- "$tmp"
 }
-export ZELLIJ_AUTO_ATTACH=true
-export ZELLIJ_AUTO_EXIT=true
-if [[ "$ALACZELL" = "true" ]]; then
-    eval "$(zellij setup --generate-auto-start zsh)"
-fi
+
+# Zoxide
 eval "$(zoxide init --cmd=cd zsh)"
 
+# Git prompt
 autoload -Uz vcs_info
 precmd() { vcs_info }
 setopt prompt_subst
 zstyle ':vcs_info:git:*' formats ' %F{magenta}(%b)%f'
 PROMPT='%F{yellow}%1~%f${vcs_info_msg_0_} ➜ '
 
+bindkey -e
+
+# Ctrl+Backspace to delete word backward
+bindkey '^H' backward-kill-word
+
+# Ctrl+Delete to delete word forward (your existing one)
 bindkey '^[[3;5~' kill-word
--- nix/configuration-gnome-xorg.nix
+++ nix/configuration-gnome-xorg.nix
@@ -1,88 +1,9 @@
 { config, lib, pkgs, ... }:
+let
+  unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
+in
 {
   imports = [ ./hardware-configuration.nix ];
-
-  # BOOT & NETWORKING
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.kernelParams = [ "nvidia.NVreg_EnableGpuFirmware=0" ];
-  boot.blacklistedKernelModules = [ "nouveau" ];
-
-  networking.hostName = "camalouu";
-  networking.networkmanager.enable = true;
-  time.timeZone = "Europe/Berlin";
-
-  # CONSOLE
-  console = {
-    font = "ter-v32n";
-    packages = [ pkgs.terminus_font ];
-    useXkbConfig = true;
-  };
-
-  programs.dconf.enable = true;
-
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
-    alsa.enable = true;
-  };
-
-  # X11 + i3
-  services.xserver = {
-    enable = true;
-    videoDrivers = [ "nvidia" ];
-
-    desktopManager = {
-      xterm.enable = false;
-    };
-    
-    windowManager.i3 = {
-      enable = true;
-      extraPackages = with pkgs; [
-        dmenu
-        i3status
-        i3lock
-      ];
-    };
-  };
-
-  services.displayManager.defaultSession = "none+i3";
-
-  programs.i3lock.enable = true;
-
-  # NVIDIA
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-  };
-
-  hardware.nvidia = {
-    modesetting.enable = true;
-    open = true;
-    powerManagement.enable = false;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-    
-    prime = {
-      sync.enable = true;
-      # offload.enable = true;
-      # offload.enableOffloadCmd = true;
-      intelBusId = "PCI:0:2:0";
-      nvidiaBusId = "PCI:1:0:0";
-    };
-  };
-
-  # ENV VARS
-  environment.variables = {
-    EDITOR = "nvim";
-  };
-
-  # FONTS
-  fonts.packages = with pkgs; [
-    nerd-fonts.fira-code
-    nerd-fonts.droid-sans-mono
-  ];
-
   # PACKAGES
   environment.systemPackages = with pkgs; [
     wget
@@ -93,64 +14,103 @@
     alacritty
     lsd
     zoxide
-    yazi
+    unstable.yazi
     neovim
     firefox
     chromium
-    zed-editor
+    unstable.zed-editor
+    mpv
     xclip
     jq
-    psmisc
-    feh  # wallpaper
-    picom  # compositor
-    pulseaudio
-    brightnessctl
-    playerctl
-    dunst        # Notification daemon
-    libnotify    # Sends the actual notifications (notify-send)
+    unzip
+    bottom
+    gnome-tweaks
+    touchegg
+    gnomeExtensions.paperwm
+    gnomeExtensions.run-or-raise
   ];
+  # BOOT & NETWORKING
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  # boot.kernelParams = [ "nvidia.NVreg_EnableGpuFirmware=0" ];
+  boot.blacklistedKernelModules = [ "nouveau" ];
+  networking.hostName = "camalouu";
+  networking.networkmanager.enable = true;
+  time.timeZone = "Europe/Berlin";
+  # CONSOLE
+  console = {
+    font = "ter-v32n";
+    packages = [ pkgs.terminus_font ];
+    useXkbConfig = true;
+  };
+  programs.dconf.enable = true;
+  services.gnome.gnome-keyring.enable = true;
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gnome ];
+  programs.nix-ld = {
+    enable = true;
+    libraries = [ pkgs.zlib pkgs.openssl ];
+  };
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+    alsa.enable = true;
+  };
+  # GNOME Xorg
+  services.xserver = {
+    enable = true;
+    videoDrivers = [ "nvidia" ];
+    displayManager.gdm = {
+      enable = true;
+      wayland = false;
+    };
+    desktopManager.gnome.enable = true;
+  };
+  # NVIDIA
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+  };
+  hardware.nvidia = {
+    modesetting.enable = true;
+    open = true;
+    powerManagement.enable = false;
+    nvidiaSettings = true;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
 
+    prime = {
+      sync.enable = true;
+      intelBusId = "PCI:0:2:0";
+      nvidiaBusId = "PCI:1:0:0";
+    };
+  };
+  # ENV VARS
+  environment.variables = {
+    EDITOR = "nvim";
+  };
+  # FONTS
+  fonts.packages = with pkgs; [
+    nerd-fonts.fira-code
+    nerd-fonts.droid-sans-mono
+  ];
   # USER
   users.users.viktor = {
     isNormalUser = true;
     extraGroups = [ "wheel" "video" "audio" ];
     shell = pkgs.zsh;
   };
-
   # ZSH
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     autosuggestions.enable = true;
     syntaxHighlighting.enable = true;
-
     shellAliases = {
       ll = "lsd -la";
       ls = "lsd";
     };
-
-    interactiveShellInit = ''
-      function y() {
-        local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
-        yazi "$@" --cwd-file="$tmp"
-        if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
-          builtin cd -- "$cwd"
-        fi
-        rm -f -- "$tmp"
-      }
-
-      eval "$(zoxide init --cmd=cd zsh)"
-
-      autoload -Uz vcs_info
-      precmd() { vcs_info }
-      setopt prompt_subst
-      zstyle ':vcs_info:git:*' formats ' %F{magenta}(%b)%f'
-      PROMPT='%F{yellow}%1~%f''${vcs_info_msg_0_} ➜ '
-
-      bindkey '^[[3;5~' kill-word
-    '';
   };
-
   nixpkgs.config.allowUnfree = true;
-  system.stateVersion = "25.11";
+  nix.settings.experimental-features = ["nix-command"];
+  system.stateVersion = "25.05";
 }
