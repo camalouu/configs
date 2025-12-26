416aa74b29c8c05e73b9c955302e42fd56b8a160
viktor <camalouu@gmail.com>
2025-12-26 17:39:00 +0100

    update nix gnome config, add VIA jorne keyboard layout

--- nix/configuration-gnome.nix
+++ nix/configuration-gnome.nix
@@ -4,65 +4,93 @@ let
 in
 {
   imports = [ ./hardware-configuration.nix ];
+
   # PACKAGES
   environment.systemPackages = with pkgs; [
+    # Network tools
     wget
     curl
     hurl
+    
+    # Development
     gh
     git
+    neovim
+    unstable.zed-editor
+    
+    # Terminal utilities
     ripgrep
     fd
     alacritty
     lsd
     zoxide
     unstable.yazi
-    neovim
-    # firefox
-    chromium
-    unstable.zed-editor
-    mpv
-    xclip
-    wl-clipboard
     jq
     unzip
     bottom
+    xclip
+    
+    # Browsers
+    chromium
+    
+    # Media
+    mpv
+    
+    # GNOME
     gnome-tweaks
     gnomeExtensions.paperwm
     gnomeExtensions.run-or-raise
+    gnomeExtensions.clipboard-indicator
+    
+    # Applications
     dialect
     speedcrunch
-    kdePackages.kdeconnect-kde
+    
+    # Keyboard tools
+    qmk
+    dos2unix
+    via
   ];
-  # BOOT & NETWORKING
+
+  # SYSTEM
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  # boot.kernelParams = [ "nvidia.NVreg_EnableGpuFirmware=0" ];
   boot.blacklistedKernelModules = [ "nouveau" ];
+  
   networking.hostName = "camalouu";
   networking.networkmanager.enable = true;
+  networking.firewall = {
+    allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
+    allowedUDPPortRanges = [ { from = 1714; to = 1764; } ];
+  };
+  
   time.timeZone = "Europe/Berlin";
+  
+  system.stateVersion = "25.05";
+
+  # NIX SETTINGS
+  nixpkgs.config.allowUnfree = true;
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+
   # CONSOLE
   console = {
     font = "ter-v32n";
     packages = [ pkgs.terminus_font ];
     useXkbConfig = true;
   };
-  programs.firefox.enable = true;
-  programs.dconf.enable = true;
-  services.gnome.gnome-keyring.enable = true;
-  xdg.portal.enable = true;
-  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gnome ];
-  programs.nix-ld = {
-    enable = true;
-    libraries = [ pkgs.zlib pkgs.openssl ];
-  };
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
-    alsa.enable = true;
-  };
-  # GNOME Xorg
+
+  # FONTS
+  fonts.packages = with pkgs; [
+    nerd-fonts.fira-code
+    nerd-fonts.droid-sans-mono
+  ];
+
+  # DISPLAY SERVER & DESKTOP
   services.xserver = {
     enable = true;
     videoDrivers = [ "nvidia" ];
@@ -72,6 +100,7 @@ in
     };
     desktopManager.gnome.enable = true;
   };
+
   # NVIDIA
   hardware.graphics = {
     enable = true;
@@ -90,22 +119,33 @@ in
       nvidiaBusId = "PCI:1:0:0";
     };
   };
-  # ENV VARS
-  environment.variables = {
-    EDITOR = "nvim";
+
+  # AUDIO
+  services.pipewire = {
+    enable = true;
+    pulse.enable = true;
+    alsa.enable = true;
   };
-  # FONTS
-  fonts.packages = with pkgs; [
-    nerd-fonts.fira-code
-    nerd-fonts.droid-sans-mono
-  ];
-  # USER
-  users.users.viktor = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" "video" "audio" ];
-    shell = pkgs.zsh;
+
+  # SERVICES
+  services.gnome.gnome-keyring.enable = true;
+  xdg.portal.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gnome ];
+
+  # HARDWARE
+  hardware.keyboard.qmk.enable = true;
+
+  # PROGRAMS
+  programs.firefox.enable = true;
+  programs.dconf.enable = true;
+  programs.kdeconnect = {
+    enable = true;
+    package = pkgs.gnomeExtensions.gsconnect;
   };
-  # ZSH
+  programs.nix-ld = {
+    enable = true;
+    libraries = [ pkgs.zlib pkgs.openssl ];
+  };
   programs.zsh = {
     enable = true;
     enableCompletion = true;
@@ -116,7 +156,17 @@ in
       ls = "lsd";
     };
   };
-  nixpkgs.config.allowUnfree = true;
-  nix.settings.experimental-features = ["nix-command"];
-  system.stateVersion = "25.05";
+
+
+  # ENVIRONMENT
+  environment.variables = {
+    EDITOR = "nvim";
+  };
+
+  # USER
+  users.users.viktor = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "video" "audio" "networkmanager" ];
+    shell = pkgs.zsh;
+  };
 }
